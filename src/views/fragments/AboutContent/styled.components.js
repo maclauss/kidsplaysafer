6acea29c58cd18929855900839d0d332cda20275
qundax <qunda.lim@outlook.sg>
2022-01-07 22:29:52 +0800

    fix(kps-6): minor code refactor

--- src/views/fragments/AboutContent/styled.components.js
+++ src/views/fragments/AboutContent/styled.components.js
@@ -1,10 +1,21 @@
 import styled from "styled-components";
 
-export const HomeContentWrapper = styled.div`
+import { ReactComponent as BackButton } from "../../assets/icons/backButton.svg";
+
+export const StyledBackButton = styled(BackButton)`
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  height: 35px;
+  width: 35px;
+  z-index: 2;
+`;
+
+export const AboutContentWrapper = styled.div`
   width: 100%;
   position: absolute;
   left: 0;
-  bottom: 50px;
+  top: 200px;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -12,35 +23,21 @@ export const HomeContentWrapper = styled.div`
 `;
 
 export const About = styled.div`
-  margin: 16px 0 5px;
+  margin-bottom: 18px;
   font-size: 60px;
   color: #db24fc;
   text-shadow: 3px 3px #42210b;
 `;
 
-export const LinksWrapper = styled.div`
-  width: 100%;
-  display: flex;
-  padding-top: 25px;
-  justify-content: space-around;
-`;
-
-export const Link = styled.button`
-  width: 80px;
-  background-color: rgba(255, 255, 255, 0.1);
-  border: none;
-  font-size: 18px;
-  color: #ffffff;
-  padding: 5px;
-`;
-
 export const ParagraphContainer = styled.div`
   overflow: scroll;
   height: 300px;
-  padding: 5px 5px 10px 5px;
+  padding: 0 20px;
 `;
 
-export const Paragraph = styled.p`
-  color: white;
-  margin: 6px 5px 15px 5px;
+export const Paragraph = styled.div`
+  margin-bottom: 8px;
+  font-size: 18px;
+  color: #ffffff;
+  text-align: justify;
 `;

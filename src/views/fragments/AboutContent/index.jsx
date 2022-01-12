0942f2f29145ad0e6e4225d3101152afecfe4ff0
qundax <qunda.lim@outlook.sg>
2022-01-12 09:45:04 +0800

    updated about page copy

--- src/views/fragments/AboutContent/index.jsx
+++ src/views/fragments/AboutContent/index.jsx
@@ -26,26 +26,25 @@ const AboutContent = () => {
         <About>About</About>
         <ParagraphContainer>
           <Paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris orci
-            nulla, blandit sit amet lorem sed, fermentum mollis metus. Nunc in
-            blandit massa. Maecenas semper urna eros, sit amet lacinia metus
-            porta ac. Sed mi nisl, maximus eget elementum in, faucibus nec
-            velit. Aliquam blandit justo ac finibus iaculis.
+            The purpose of this quiz is to better understand how your kids would
+            behave when it comes to online gaming.
+          </Paragraph>
+          <Paragraph>How it works:</Paragraph>
+          <Paragraph>1) Enter a username for your child and yourself</Paragraph>
+          <Paragraph>2) Select an avatar</Paragraph>
+          <Paragraph>3) Click 'Start" to begin the 5-question quiz</Paragraph>
+          <Paragraph>
+            4) For each question, let your child answer first, then record your
+            response after
           </Paragraph>
           <Paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris orci
-            nulla, blandit sit amet lorem sed, fermentum mollis metus. Nunc in
-            blandit massa. Maecenas semper urna eros, sit amet lacinia metus
-            porta ac. Sed mi nisl, maximus eget elementum in, faucibus nec
-            velit. Aliquam blandit justo ac finibus iaculis.
+            5) At the end of the quiz, the results will be compared
           </Paragraph>
           <Paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris orci
-            nulla, blandit sit amet lorem sed, fermentum mollis metus. Nunc in
-            blandit massa. Maecenas semper urna eros, sit amet lacinia metus
-            porta ac. Sed mi nisl, maximus eget elementum in, faucibus nec
-            velit. Aliquam blandit justo ac finibus iaculis.
+            Use this opportunity to engage with your child. Ask them why they
+            picked the particular option before proceeding to the next question.
           </Paragraph>
+          <Paragraph>Their answers might surprise you.</Paragraph>
         </ParagraphContainer>
       </AboutContentWrapper>
     </>

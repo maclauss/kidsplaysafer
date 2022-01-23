97b4b5b8d90336a25bfb1b5cbe58af3bc9267e69
qundax <qunda.lim@outlook.sg>
2022-01-23 14:33:50 +0800

    feature(kps-23): added play again desktop page

--- src/views/pages/PlayAgain/index.jsx
+++ src/views/pages/PlayAgain/index.jsx
@@ -1,41 +1,36 @@
 import { useScreenSize } from "../../../hooks/useScreenSize";
 
-import Logo from "../../assets/icons/logo.png";
-
 import {
   PlayAgainWrapper,
-  StyledLogo,
   StyledTop,
   StyledBottom,
+  StyledDesktopTop,
+  StyledConfetti,
+  DesktopBackground,
 } from "./styled.components";
+import Navbar from "../../fragments/Navbar";
 import PlayAgainContent from "../../fragments/PlayAgainContent";
 
 const PlayAgain = () => {
   const { isSmallScreen } = useScreenSize();
   return (
     <PlayAgainWrapper>
-      {!isSmallScreen && (
-        <div
-          style={{
-            position: "absolute",
-            top: "50vh",
-            left: "50vw",
-            transform: "translate(-50%, -50%)",
-            fontSize: "60px",
-            textAlign: "center",
-          }}
-        >
-          <StyledLogo src={Logo} />
-          <div>Desktop version coming soon!</div>
-        </div>
-      )}
-      {isSmallScreen && (
+      {isSmallScreen ? (
         <>
           <StyledTop />
           <StyledBottom />
-          <PlayAgainContent />
+        </>
+      ) : (
+        <>
+          <Navbar>
+            <StyledDesktopTop />
+          </Navbar>
+          <StyledConfetti />
+          <DesktopBackground />
         </>
       )}
+
+      <PlayAgainContent />
     </PlayAgainWrapper>
   );
 };
--- src/views/pages/PlayAgain/styled.components.js
+++ src/views/pages/PlayAgain/styled.components.js
@@ -3,16 +3,14 @@ import styled from "styled-components";
 import Background from "../../assets/icons/background.png";
 import { ReactComponent as Top } from "../../assets/icons/logoTop.svg";
 import { ReactComponent as Bottom } from "../../assets/icons/bottom.svg";
+import { ReactComponent as DesktopTop } from "../../assets/icons/desktopLogoTop.svg";
+import { ReactComponent as desktopConfetti } from "../../assets/icons/desktopConfetti.svg";
+import { ReactComponent as consoleBackground } from "../../assets/icons/consoleBackground.svg";
 
 export const PlayAgainWrapper = styled.div`
   height: 100vh;
   background-image: url(${Background});
   background-size: cover;
-`;
-
-export const StyledLogo = styled.img`
-  height: 400px;
-  width: 400px;
 `;
 
 export const StyledTop = styled(Top)`
@@ -25,4 +23,22 @@ export const StyledBottom = styled(Bottom)`
   position: absolute;
   bottom: -16px;
   left: 0;
+`;
+
+export const StyledDesktopTop = styled(DesktopTop)`
+  width: 500px;
+`;
+
+export const StyledConfetti = styled(desktopConfetti)`
+  width: 1200px;
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+`;
+
+export const DesktopBackground = styled(consoleBackground)`
+  position: absolute;
+  left: 50%;
+  top: 60px;
+  transform: translateX(-50%);
 `;
